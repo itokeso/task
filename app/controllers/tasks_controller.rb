214bc51b4cf86f95066f36e0077d80b9802ffd74
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-18 21:52:45 +0900

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,18 @@ class TasksController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+  end
+
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :done)
+  end
 end
